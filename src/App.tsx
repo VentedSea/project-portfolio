dd8b11a6da8b67526bd412075024d083ae3a0596
Richard Sims <richievsims@gmail.com>
2024-12-27 22:46:06 +0200

    styling updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styles from './App.module.css'
+
+const Tabs = {
+  Home: 'Home',
+  AboutMe: 'About me',
+  Projects: 'Projects'
+}
+const Header = ({ currentTab, setCurrentTab }: {currentTab: string, setCurrentTab: (item:string)=>void}) =>{
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerFlex}>
+        <div className={styles.tabBox} >
+          <div className={styles.headerButton} onClick={()=>setCurrentTab(Tabs.AboutMe)}>{Tabs.AboutMe}</div>
+          <div className={`${styles.headerSlider} ${currentTab === Tabs.AboutMe ? styles.showSlider : ''}`}/>
+        </div>
+        <div className={styles.tabBox} >
+          <div className={styles.headerButton} onClick={()=>setCurrentTab(Tabs.Home)}>{Tabs.Home}</div>
+          <div className={`${styles.headerSlider} ${currentTab === Tabs.Home ? styles.showSlider : ''}`}/>
+        </div>
+        <div className={styles.tabBox} >
+          <div className={styles.headerButton} onClick={()=>setCurrentTab(Tabs.Projects)}>{Tabs.Projects}</div>
+          <div className={`${styles.headerSlider} ${currentTab === Tabs.Projects ? styles.showSlider : ''}`}/>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentTab, setCurrentTab] = useState(Tabs.Home);
 
   return (
     <>
+    <Header currentTab={currentTab} setCurrentTab={setCurrentTab}/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
